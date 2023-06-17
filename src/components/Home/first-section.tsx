9bfa1ece1039d17b9d50b8ecfeae3b2e0b965631
Henri <98414850+ProHen9@users.noreply.github.com>
2023-06-17 23:25:46 +0200

    erste Seiten für mainz.news

---
+++ src/components/Home/first-section.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+        <>
+            <h2>Hey ✌️, Willkommen auf mainz.news!</h2>
+        </>
+    )
+})

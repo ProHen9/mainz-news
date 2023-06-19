86eaaf59ebc297332d1d4568b5c8d9740d990ac6
Henri <98414850+ProHen9@users.noreply.github.com>
2023-06-19 16:33:11 +0200

    mainz.news erste grundbausteine

--- src/components/Home/first-section.tsx
+++ src/components/Home/first-section.tsx
@@ -1,9 +1,11 @@
-import { component$ } from "@builder.io/qwik";
-
+import { component$, useStyles$ } from "@builder.io/qwik";
+import styles from "./first-section.css?inline"
 export default component$(() => {
+    useStyles$(styles)
     return (
-        <>
+        <div class="container">
             <h2>Hey ✌️, Willkommen auf mainz.news!</h2>
-        </>
+            <p>Infos aus Mainz und aus der Umgbung</p>
+        </div>
     )
 })
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import {  type DocumentHead } from "@builder.io/qwik-city";
 import { component$ } from "@builder.io/qwik";
 import FirstSection from "~/components/Home/first-section";
-
+import SecondSection from "../components/Home/second-section"
 export default component$(() => {
   return (
     <>
       <FirstSection />
+      <SecondSection />
     </>
   )
 })
